1b488c915f86491105ad96e7ea9686363c99c974
Froizel Rej Apolonio <froizelrej@gmail.com>
2023-06-28 20:52:43 +0800

    commit

--- script.js
+++ script.js
@@ -1,105 +1,105 @@
-// Initialize WebAR.rocks.object
-var rocksObject = new WebARRocksObject({
-    // Set up your configuration options here
-  });
+// // Initialize WebAR.rocks.object
+// var rocksObject = new WebARRocksObject({
+//     // Set up your configuration options here
+//   });
   
-  // Variables to store the reference data and tracking state
-  var referenceData = null;
-  var isTracking = false;
+//   // Variables to store the reference data and tracking state
+//   var referenceData = null;
+//   var isTracking = false;
   
-  // Function to handle the scanning step
-  function scanObject() {
-    // Access the device camera
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(function (stream) {
-        // Create a video element to display the camera feed
-        var video = document.createElement('video');
-        video.srcObject = stream;
-        video.play();
+//   // Function to handle the scanning step
+//   function scanObject() {
+//     // Access the device camera
+//     navigator.mediaDevices.getUserMedia({ video: true })
+//       .then(function (stream) {
+//         // Create a video element to display the camera feed
+//         var video = document.createElement('video');
+//         video.srcObject = stream;
+//         video.play();
   
-        // Create a canvas element to capture images
-        var canvas = document.createElement('canvas');
-        var context = canvas.getContext('2d');
+//         // Create a canvas element to capture images
+//         var canvas = document.createElement('canvas');
+//         var context = canvas.getContext('2d');
   
-        // Set up a timer to capture multiple frames
-        var numFrames = 5; // Number of frames to capture
-        var delayBetweenFrames = 500; // Delay in milliseconds between frames
+//         // Set up a timer to capture multiple frames
+//         var numFrames = 5; // Number of frames to capture
+//         var delayBetweenFrames = 500; // Delay in milliseconds between frames
   
-        var captureFrame = function () {
-          context.drawImage(video, 0, 0, canvas.width, canvas.height);
-          var imageData = canvas.toDataURL('image/jpeg');
+//         var captureFrame = function () {
+//           context.drawImage(video, 0, 0, canvas.width, canvas.height);
+//           var imageData = canvas.toDataURL('image/jpeg');
   
-          // Save the captured image data as referenceData
-          if (!referenceData) {
-            referenceData = [];
-          }
-          referenceData.push(imageData);
+//           // Save the captured image data as referenceData
+//           if (!referenceData) {
+//             referenceData = [];
+//           }
+//           referenceData.push(imageData);
   
-          numFrames--;
-          if (numFrames > 0) {
-            setTimeout(captureFrame, delayBetweenFrames);
-          } else {
-            // Once the scanning step is complete, set isTracking to true
-            isTracking = true;
-            stream.getTracks()[0].stop(); // Stop the camera stream
-            initializeTracking(); // Start object detection and tracking
-          }
-        };
+//           numFrames--;
+//           if (numFrames > 0) {
+//             setTimeout(captureFrame, delayBetweenFrames);
+//           } else {
+//             // Once the scanning step is complete, set isTracking to true
+//             isTracking = true;
+//             stream.getTracks()[0].stop(); // Stop the camera stream
+//             initializeTracking(); // Start object detection and tracking
+//           }
+//         };
   
-        // Start capturing frames
-        setTimeout(captureFrame, delayBetweenFrames);
-      })
-      .catch(function (error) {
-        console.log('Camera access error:', error);
-      });
-  }
+//         // Start capturing frames
+//         setTimeout(captureFrame, delayBetweenFrames);
+//       })
+//       .catch(function (error) {
+//         console.log('Camera access error:', error);
+//       });
+//   }
   
-  // Function to initialize object detection and tracking
-  function initializeTracking() {
-    if (referenceData) {
-      rocksObject.load(referenceData, function () {
-        // Callback function when reference data is loaded successfully
-        console.log('Reference data loaded successfully.');
+//   // Function to initialize object detection and tracking
+//   function initializeTracking() {
+//     if (referenceData) {
+//       rocksObject.load(referenceData, function () {
+//         // Callback function when reference data is loaded successfully
+//         console.log('Reference data loaded successfully.');
   
-        // Start object detection and tracking
-        rocksObject.track(function (err, result) {
-          // Callback function for tracking updates
-          if (result) {
-            // Object detected and tracked
-            console.log('Object detected and tracked:', result);
-            // Overlay your desired content on top of the tracked object
-            // Use libraries like Three.js or WebGL for rendering the content
-          } else {
-            // Object lost or tracking error
-            console.log('Object lost or tracking error:', err);
-          }
-        });
-      });
-    } else {
-      console.log('Reference data not available. Perform object scanning first.');
-    }
-  }
+//         // Start object detection and tracking
+//         rocksObject.track(function (err, result) {
+//           // Callback function for tracking updates
+//           if (result) {
+//             // Object detected and tracked
+//             console.log('Object detected and tracked:', result);
+//             // Overlay your desired content on top of the tracked object
+//             // Use libraries like Three.js or WebGL for rendering the content
+//           } else {
+//             // Object lost or tracking error
+//             console.log('Object lost or tracking error:', err);
+//           }
+//         });
+//       });
+//     } else {
+//       console.log('Reference data not available. Perform object scanning first.');
+//     }
+//   }
   
-  // Function to handle the submission of augmented content
-  function submitContent(event) {
-    event.preventDefault();
-    var contentInput = document.getElementById('contentInput');
-    var augmentedContent = contentInput.value;
+//   // Function to handle the submission of augmented content
+//   function submitContent(event) {
+//     event.preventDefault();
+//     var contentInput = document.getElementById('contentInput');
+//     var augmentedContent = contentInput.value;
   
-    // Save the augmented content as the reference data for object tracking
-    referenceData = augmentedContent;
+//     // Save the augmented content as the reference data for object tracking
+//     referenceData = augmentedContent;
   
-    // Clear the input field after submission
-    contentInput.value = '';
+//     // Clear the input field after submission
+//     contentInput.value = '';
   
-    // If object tracking is already initiated, reload the reference data
-    if (isTracking) {
-      rocksObject.load(referenceData, function () {
-        console.log('Reference data loaded successfully.');
-      });
-    }
-  }
+//     // If object tracking is already initiated, reload the reference data
+//     if (isTracking) {
+//       rocksObject.load(referenceData, function () {
+//         console.log('Reference data loaded successfully.');
+//       });
+//     }
+//   }
   
-  // Call the scanObject function to initiate the scanning step
-  scanObject();
+//   // Call the scanObject function to initiate the scanning step
+//   scanObject();
   
